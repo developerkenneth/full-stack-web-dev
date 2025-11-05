937a6e95fb966550ba329e77eebce2ede3c16395
Kenneth Otis <developerkenneth9696@gmail.com>
2025-11-05 11:21:15 +0100

    we added username validation today 11/5/2025 .... we go to the next class

--- regularExpression/app.js
+++ regularExpression/app.js
@@ -1,10 +1,62 @@
 const pattern = /[a-z]/;
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+const username = document.querySelector("#username");
+const usernameError = document.querySelector("#username-error");
+const password = document.querySelector("#password");
 
-// /[0-9]/
-const string = "hi guys 1";
+const students = [
+  "Onyiye",
+  "Chinecherem",
+  "Gloria",
+  "Somto",
+  "Marvellous",
+  "ThankGod",
+  "Akachukwu",
+];
 
-if(pattern.test(string)){
-    alert("passed");
-}else{
-  alert("did not pass");
+
+function isEmpty(string){
+    return string.length < 1 ? true : false ;
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let error = false;
+  const userEmail = email.value;
+  const userPassword = password.value;
+  const usernameData = username.value;
+
+  if (isEmpty(userEmail)) {
+    emailError.textContent = "please email cannot be empty";
+    error = true;
+  }
+
+  // username validation
+
+  if (isEmpty(usernameData)) {
+    usernameError.textContent = "Please enter a username";
+    error = true;
+  } else {
+    const result = students.find(function (student) {
+      return student.toLowerCase().includes(usernameData.toLowerCase());
+    });
+
+
+    if (result) {
+        usernameError.textContent = `This username already exist ${usernameData}, try another username`;
+        error = true;
+    }
+  }
+
+  if (!error) {
+    document.querySelectorAll(".error").forEach((e) => {
+      e.textContent = "";
+    });
+
+    document.querySelectorAll(".input").forEach((i)=>{
+      i.value = "";
+    })
+  }
+});
